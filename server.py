82724b3263f5f401115b581e24d32ae5e23c1613
Bheeman <bheeman@umich.edu>
2026-06-10 11:12:59 -0400

    gather hex and vertex info

--- server.py
+++ server.py
@@ -10,6 +10,82 @@ ALLOWED_ORIGIN = 'https://colonist.io'
 
 out_sequence = 1
 
+
+class Hex:
+    def __init__(self, hex_id, resource, dice):
+        self.hex_id = hex_id
+        self.resource = resource  # 0=desert, 1=lumber, 2=brick, 3=wool, 4=grain, 5=ore
+        self.dice = dice          # 2-12, 0 for desert
+
+board = [None] * 19
+
+VERTEX_TO_HEXES = {
+    0:  [0],
+    1:  [0, 11],
+    2:  [0, 11, 12],
+    3:  [0, 1, 12],
+    4:  [0, 1],
+    5:  [0],
+    6:  [1, 12, 13],
+    7:  [1, 2, 13],
+    8:  [1, 2],
+    9:  [1],
+    10: [2, 3, 13],
+    11: [2, 3],
+    12: [2],
+    13: [2],
+    14: [3, 13, 14],
+    15: [3, 4, 14],
+    16: [3, 4],
+    17: [3],
+    18: [4, 5, 14],
+    19: [4, 5],
+    20: [4],
+    21: [4],
+    22: [5, 14, 15],
+    23: [5, 6, 15],
+    24: [5, 6],
+    25: [5],
+    26: [6, 7, 15],
+    27: [6, 7],
+    28: [6],
+    29: [6],
+    30: [7, 8, 16],
+    31: [7, 8],
+    32: [7],
+    33: [7, 15, 16],
+    34: [8, 9],
+    35: [8],
+    36: [8],
+    37: [8, 9, 16],
+    38: [9, 10],
+    39: [9],
+    40: [9, 16, 17],
+    41: [9, 10, 17],
+    42: [10],
+    43: [10],
+    44: [10, 11, 17],
+    45: [10, 11],
+    46: [11],
+    47: [11, 12, 17],
+    48: [12, 17, 18],
+    49: [12, 13, 18],
+    50: [13, 14, 18],
+    51: [14, 15, 18],
+    52: [15, 16, 18],
+    53: [16, 17, 18],
+}
+
+def parse_board(data):
+    tile_hex_states = data.get('data', {}).get('payload', {}).get('gameState', {}).get('mapState', {}).get('tileHexStates', {})
+    for hex_id, hex_data in tile_hex_states.items():
+        board[int(hex_id)] = Hex(
+            hex_id=int(hex_id),
+            resource=hex_data.get('type'),
+            dice=hex_data.get('diceNumber')
+        )
+
+
 def check_origin():
     origin = request.headers.get('Origin', '')
     # allow empty origin (GM_xmlhttpRequest) and colonist.io
@@ -39,8 +115,11 @@ def handle_incoming():
 
     msg_type = data.get('data', {}).get('type')
 
+    if msg_type == 4:
+        parse_board(data)
+
     #placement settlements
-    if msg_type == 30:
+    else if msg_type == 30:
 #         return jsonify({
 # #             "actions": [
 # #                 {"action": 66, "payload": 28, "sequence": next_out_sequence()},
